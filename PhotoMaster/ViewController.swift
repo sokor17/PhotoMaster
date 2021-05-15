5fbea7fa6572e8b4fe3a1b2898bc5eb5c6e4d1a1
YuKono <cosmos17128@gmail.com>
2021-05-16 00:42:34 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by Yu Kono on 2021/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
